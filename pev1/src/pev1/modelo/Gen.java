bc747ed449b59c9c3a175c78403e95e5193bdb50
Eli <eaguero@ucm.es>
2018-02-20 17:13:22 +0100

    Primera sesion

---
+++ pev1/src/pev1/modelo/Gen.java
@@ -0,0 +1,14 @@
+package pev1.modelo;
+
+public class Gen {
+	private boolean[] alelos = new boolean[2];
+	
+	public Gen() {
+		
+		this.alelos[0] = true;
+		this.alelos[1] = false;
+		
+	}
+
+	
+}
---
+++ pev1/src/pev1/modelo/IndividuoProblemaConcreto.java
@@ -0,0 +1,5 @@
+package pev1.modelo;
+
+public class IndividuoProblemaConcreto {
+
+}
